96419943298b15d5bb2ec7f12dda757d099d6456
Crawford Currie <git@c-dot.co.uk>
2015-08-01 20:48:38 +0100

    Bits

---
+++ TestStore.uncompressed.js
@@ -0,0 +1,32 @@
+/* Copyright (C) 2015 Crawford Currie http://c-dot.co.uk / MIT */
+
+/**
+ * A store engine using HTML5 localStorage.
+ * @implements AbstractStore
+ */
+function TestStore(params) {
+    "use strict";
+    params.user = "TEST";
+    LocalStorageStore.call(this, params);
+}
+
+const SQUIRREL_STORE = TestStore;
+
+TestStore.prototype = Object.create(LocalStorageStore.prototype);
+
+TestStore.prototype.identifier = function() {
+    "use strict";
+
+    return "Test";
+};
+
+TestStore.prototype.read = function(path, ok, fail) {
+    "use strict";
+    LocalStorageStore.prototype.read(this, "TEST" + path, ok, fail);
+};
+
+TestStore.prototype.write = function(path, data, ok, fail) {
+    "use strict";
+
+   LocalStorageStore.prototype.write(this, "TEST" + path, data, ok, fail);
+};
